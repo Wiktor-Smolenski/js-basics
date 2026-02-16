ffcf6cb0aaf37c7866aa86d79e0094fad49268fe
Wiktor Smolenski <wiktorsmolenski@gmail.com>
2026-02-16 16:19:12 +0100

    Remove old variables file and update exercises

--- 01_StringsAndVariablesExercises.js
+++ 01_StringsAndVariablesExercises.js
@@ -1,127 +1,189 @@
-const a = 10;
-const b = 3;
+console.log("Hello World!");
+//Adding two variables and making basic operations with them
+console.log("This is the first exercise with variables and basic operations");
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
+let a = 10;
+let b = 10;
 
+console.log("a=", a);
+console.log("b=", b);
+console.log("(a + b) =", a + b);
+console.log("(a - b) =", a - b);
+console.log("(a * b) =", a * b);
+console.log("(a / b) =", a / b);
+
+//Basic if statement and for loop
+console.log("This is result of exercise in which I want to check which variable is bigger");
 if (a > b) {
   console.log("a is bigger");
-} else {
+} else if (b > a) {
   console.log("b is bigger");
+} else {
+  console.log("a and b are equal");
 }
-for (let i = 4; i <= 5; i++) {
-  console.log(i);
-}
-let variable = "moge tu dac co chce";
-console.log(variable);
+
+//Adding two strings together with many ways to do it
+console.log("What's your name?");
 let firstName = "Wiktor";
 let lastName = "Smoleński";
-console.log(firstName + " " + lastName); //way to add strings together
-let greeting = "Siema";
-greeting += " Buzia";
-console.log(greeting);
-let imie = "Wiktor";
-let powitanie = "Siema";
-console.log(powitanie, imie, "i tutaj moge kazdy tekst dac", "z spacjami trzeba walczyc");
+console.log(firstName + " " + lastName);
+let greeting = "Hey ";
+greeting += firstName;
+console.log(greeting, "nice to meet you", "hope you will have great coding session today :)");
 
-let number = 17; //odd or even
+//few exercises with variables and strings
+console.log("Here are results of few exercises with variables and strings");
+//odd or even number
+console.log("In this exercise I can choose and number and check if it's odd or even");
+ let number = 17;
 if ((number%2) === 0) {
     console.log(number, "is even");
 }
 else {
     console.log(number, "is odd");
 }
-let n = 4; // print sum 1 + 2 + 3 + ... + n
+//print sum of 1 + 2 + 3 + ... + n
+console.log("In this exercise I can choose a number n and find the sum of 1 + 2 + 3 + ... + n");
+let n = 4;
 let sum = 0;
 for (let i=1; i<=n; i++) {
     sum += i;
 }
-console.log("sum of 1 to", n, "is", sum);
+console.log("sum of numbers from \"1\" to", n, "is", sum);
 
+//find the biggest of three numbers
+console.log("In this exercise I can choose three numbers and find the biggest one");
 let x = 5;
+console.log(x, "this is the first number");
 let y = 12;
+console.log(y, "this is the second number");
 let z = 9;
+console.log(z, "this is the third number");
  let biggest = "is the biggest";
 if (x>y && x>z) {
-    console.log(x, biggest);
+    console.log("x = ", x, biggest);
 }
 else if (y>x && y>z) {
-    console.log(y, biggest);
+    console.log("y = ", y, biggest);
 }
 else {
-    console.log(z, biggest);
+    console.log("z = ", z, biggest);
 }
-let name = "Wiktor";
-console.log(name[3], "way to choose letter in a word"); // do wybierania ktora litere slowa chcemy wyswietlic
-console.log(name.length, "this is the length of the name");
+
+//string exercises on a word
+console.log("Here is the word I will be working with");
+let word = "JavaScript";
+console.log(word);
+
+//choosing exact letter from a word
+let i = 3;
+if (i <= word.length) {
+console.log(i, "th letter of the word", word, "is", word[i-1]);
+}
+else {
+    console.log("There is no", i, "th letter in the word", word);
+}
+//finding the length of the word
+console.log(word.length, "letters is the length of the word");
+
+//reverse the word
+console.log ("Now I will print the word in reverse order");
 let reversed1 = "";
-for (let i = name.length -1; i>=0; i--) {
-    reversed1 += name[i];
+for (let i = word.length -1; i>=0; i--) {
+    reversed1 += word[i];
 }
-console.log(reversed1, "this is the name reversed");
-let reversed2 = "";
-let text = "JavaScript";
-for (let j = text.length -1; j>=0; j--) {
-    reversed2 += text[j];
-}
-console.log(reversed2, "this is the text reversed");
-let word2 = "programming";
+console.log(reversed1);
+
+//checkin for number of vowels in the word
 let vowelSum = 0;
-for (let h = word2.length -1; h>=0; h--) {
-    if (word2[h] === "a" || word2[h] === "e" || word2[h] === "i" || word2[h] === "o" || word2[h] === "u") {
+for (let h = word.length -1; h>=0; h--) {
+    if (word[h] === "a" || word[h] === "e" || word[h] === "i" || word[h] === "o" || word[h] === "u") {
        vowelSum++;
 }
 }
-console.log(vowelSum, "this is the number of vowels in the word");
-console.log(typeof vowelSum, "this is the type of the variable vowelSum");
+console.log(vowelSum, "this is the number of vowels in the word", word);
+
+//a try to check te location of those vowels in the word
+console.log("Now I will print the locations of these vowels in the word");
+/*
+//letter a
+for (let a = 0; a<=word.length; a++) {
+    if (word[a] === "a") {
+        console.log("a is located at", a+1);
+    }
+}
+if (word.indexOf("a") === -1) {
+    console.log("There is no a in the word", word);
+}
+
+//letter e
+for (let e = 0; e<=word.length; e++) {
+    if (word[e] === "e") {
+        console.log("e is located at", e+1);
+    }
+}
+if (word.indexOf("e") === -1) {
+    console.log("There is no e in the word", word);
+}
+
+//letter i
+for (let i = 0; i<=word.length; i++) {
+    if (word[i] === "i") {
+        console.log("i is located at", i+1);
+    }
+}
+if (word.indexOf("i") === -1) {
+    console.log("There is no i in the word", word);
+}
+
+//letter o
+for (let o = 0; o<=word.length; o++) {
+    if (word[o] === "o") {
+        console.log("o is located at", o+1);
+    }
+}
+if (word.indexOf("o") === -1) {
+    console.log("There is no o in the word", word);
+}
+
+//letter u
+for (let u = 0; u<=word.length; u++) {
+    if (word[u] === "u") {
+        console.log("u is located at", u+1);
+    }
+}
+if (word.indexOf("u") === -1) {
+    console.log("There is no u in the word", word);
+}
+
+//letter y
+for (let y = 0; y<=word.length; y++) {
+    if (word[y] === "y") {
+        console.log("y is located at", y+1);
+    }
+}
+if (word.indexOf("y") === -1) {
+    console.log("There is no y in the word", word);
+}
+*/
+//there is a faster way to do it
+let vowels = ["a", "e", "i", "o", "u", "y"];
+for (let v= 0; v<word.length; v++) {
+    if (vowels.includes(word[v])) {
+        console.log (word[v], "is located at", v+1);
+    }
+} 
+
+//a way to check the type of variable
+typeof vowelSum;
+
+//a way to add a new property to an object
 let kot = {
     age: 10,
     name: "Tequila",
     color: "grey",
-    speak: function() {
+    speak()
+    {
         console.log("Hi there!");
     }
-}
-
-const botName = "teacherBot";
-greeting = `My name is ${botName}.`;
-console.log(greeting);
-
-const subject = "JavaScript";
-const topic = "strings";
-
-const sentence = `Today, you will learn about ${topic} in ${subject}.`;
-console.log(sentence);
-
-const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
-console.log(strLengthIntro);
-
-console.log(subject.length);
-
-console.log(`Here is an example of using the length property on the word ${topic}.`);
-console.log(topic.length);
-
-console.log(`Here is an example of accessing the first letter in the word ${subject}.`);
-
-console.log(subject[0]);
-
-console.log(`Here is an example of accessing the second letter in the word ${subject}.`);
-console.log(subject[1]);
-
-console.log(`Here is an example of accessing the last letter in the word ${subject}.`);
-
-const lastCharacter = subject[subject.length - 1];
-console.log(lastCharacter);
-
-const learningIsFunSentence = "Learning is fun.";
-
-console.log("Here are examples of finding the positions of substrings in the sentence.");
-
-console.log(learningIsFunSentence.indexOf("Learning"));
-
-console.log(learningIsFunSentence.indexOf("fun"));
-console.log(learningIsFunSentence.indexOf("learning"));
-
-console.log("I hope you enjoyed learning today.");
+};
