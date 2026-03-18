2b80c591dc5a1f7ff313a18add144e91af5ed7eb
Wiktor Smolenski <wiktorsmolenski@gmail.com>
2026-03-18 15:51:44 +0100

    worked with object for past days

--- 04_ArraysExercises.js
+++ 04_ArraysExercises.js
@@ -40,3 +40,81 @@ shoppingList.shift();
 shoppingList[0] = "Canola Oil";
 
 console.log(getShoppingListMsg(shoppingList));
+
+// lunch menu exercises
+const lunches = [];
+
+function addLunchToEnd(lunches, string) {
+  console.log(string + " added to the end of the lunch menu.");
+  lunches.push(string);
+  return lunches;
+}
+
+function addLunchToStart(lunches, string) {
+  console.log(string + " added to the start of the lunch menu.");
+  lunches.unshift(string);
+  return lunches;
+}
+
+function removeLastLunch(lunches) {
+  if (lunches == 0) {
+    console.log("No lunches to remove.")
+  } else {
+    console.log(lunches.pop() + " removed from the end of the lunch menu.");
+  } 
+  return lunches;
+}
+
+function removeFirstLunch(lunches) {
+  if (lunches == 0) {
+    console.log("No lunches to remove.");
+  } else {
+    console.log(lunches.shift() + " removed from the start of the lunch menu.");
+  }
+  return lunches;
+}
+
+function getRandomLunch(lunches) {
+  if (lunches == 0) {
+    console.log("No lunches available.");
+  } else {
+    let i = lunches.length;
+    let j = (Math.floor(Math.random() * i));
+    let randomLunch = lunches[j];
+    console.log("Randomly selected lunch: " + randomLunch);
+  }
+  return lunches;
+}
+
+function showLunchMenu(lunches) {
+  if (lunches == 0) {
+    console.log("The menu is empty.");
+  } else {
+    console.log("Menu items: " + lunches.join(", "));
+  }
+  return lunches;
+}
+
+//golf counter
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes == par - 1) {
+    return names[2];
+  } else if (strokes == par) {
+    return names[3];
+  } else if (strokes == par + 1) {
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
+  } else if (strokes >= par + 3) {
+    return names[6];
+  }
+  return "dupa";
+}
+
+golfScore(1, 1);
---
+++ 05_ObjectsExercises.js
@@ -0,0 +1,168 @@
+
+//recipes book with objects
+const recipes = [];
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+const recipe2 = {
+  name: "Chicken Curry",
+  ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+const recipe3 = {
+  name: "Vegetable Stir Fry",
+  ingredients: ["broccoli", "carrot", "bell pepper"],
+  cookingTime: 15,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+recipes.push(recipe1, recipe2, recipe3);
+
+function getTotalIngredients(ingredients) {
+  return ingredients.length;
+}
+
+function getDifficultyLevel(cookingTime) {
+  if (cookingTime <= 30) {
+    return "easy";
+  } else if (cookingTime <= 60) {
+    return "medium";
+  } else {
+    return "hard";
+  }
+}
+
+const recipe1TotalIngredients = getTotalIngredients(recipe1.ingredients);
+console.log(recipe1TotalIngredients);
+
+const recipe1DifficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+console.log(recipe1DifficultyLevel);
+
+recipe1.totalIngredients = getTotalIngredients(recipe1.ingredients);
+recipe1.difficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+
+recipe2.totalIngredients = getTotalIngredients(recipe2.ingredients);
+recipe2.difficultyLevel = getDifficultyLevel(recipe2.cookingTime);
+
+recipe3.totalIngredients = getTotalIngredients(recipe3.ingredients);
+recipe3.difficultyLevel = getDifficultyLevel(recipe3.cookingTime);
+
+console.log(recipes);
+
+//quiz game with objects
+
+const questions = [
+{
+  category: "muzyka",
+  question: "pytam o muzyke?",
+  choices: ["muzyka1", "muzyka2", "muzyka3"],
+  answer: "muzyka2",
+},
+{
+  category: "sport",
+  question: "pytam o sport?",
+  choices: ["sport1", "sport2", "sport3"],
+  answer: "sport1",
+},
+{
+  category: "matematyka",
+  question: "pytam o matematyke?",
+  choices: ["matematyka1", "matematyka2", "matematyka3"],
+  answer: "matematyka3",
+},
+{
+  category: "fizyka",
+  question: "pytam o fizyke?",
+  choices: ["fizyka1", "fizyka2", "fizyka3"],
+  answer: "fizyka2",
+},
+{
+  category: "chemia",
+  question: "pytam o chemie?",
+  choices: ["chemia1", "chemia2", "chemia3"],
+  answer: "chemia3",
+}
+];
+
+function getRandomQuestion(questions) {
+  let i = Math.floor(Math.random() * questions.length);
+  return questions[i];
+}
+
+let a = getRandomQuestion(questions);
+let b = a.choices;
+
+function getRandomComputerChoice(b) {
+  let i = Math.floor(Math.random() * b.length);
+  return b[i];
+}
+
+let c = getRandomComputerChoice(b);
+
+function getResults(a, c) {
+  let i = a.answer;
+  if (i === c) {
+    return "The computer's choice is correct!";
+  } else {
+    return "The computer's choice is wrong. The correct answer is: " + i;
+  }
+}
+
+console.log(a);
+console.log("Computer's choice: " + c);
+console.log(getResults(a, c));
+
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// record collection update function
+function updateRecords(records, id, prop, value) {
+  let a = records[id];
+  if (value === "") {
+    delete a[prop]; //number 2
+  } else if (prop !== "tracks" && value !== ""){
+    a[prop] = value; // number 3
+  } else if (prop === "tracks" && value !== "" && a.hasOwnProperty(prop) === false) {
+    a[prop] = [value]; //number 4
+  } else if(prop === "tracks" && value !== "") {
+    a[prop].push(value); // number 5
+  }else {
+    return "conditions not met";
+  }
+  return records;
+}
+
+console.log(updateRecords(recordCollection, 5439, "artist", "ABBA"));
+console.log(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"));
+console.log(updateRecords(recordCollection, 2548, "artist", ""));
+console.log(updateRecords(recordCollection, 1245, "tracks", "Addicted to Love"));
+console.log(updateRecords(recordCollection, 2468, "tracks", "Free"));
+console.log(updateRecords(recordCollection, 2548, "tracks", ""));
+console.log(updateRecords(recordCollection, 1245, "albumTitle", "Riptide"));
