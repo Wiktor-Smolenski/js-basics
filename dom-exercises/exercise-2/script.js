1e31554ba5bd51d33aafc2835b1d62bde1905ab3
Wiktor Smolenski <wiktorsmolenski@gmail.com>
2026-04-29 19:09:19 +0200

    cleanup and few DOM practices

---
+++ dom-exercises/exercise-2/script.js
@@ -0,0 +1,18 @@
+const characters = document.getElementById("text-input");
+
+const print = document.getElementById("char-count");
+
+
+
+characters.addEventListener("input", insertLength);
+
+function insertLength() {
+
+  if (characters.value.length > 50) {
+    characters.value = characters.value.substring(0, 50);
+  }
+
+  const len = characters.value.length;
+  print.textContent = `Character Count: ${len}/50`;
+  print.classList.toggle("over-limit", len >= 50);
+}
